40d3898cae4dc06f8412d96602e59d64cda40a51
jamesmwangi55 <jamesmwangi55@gmail.com>
2024-05-10 10:57:29 +0300

    Add data to be used by application

---
+++ backend/src/data/books.ts
@@ -0,0 +1,602 @@
+export const booksData = [
+    {
+        "title": "Curious Princess and the Enchanted Garden",
+        "author": "Reese Smith",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Clever Monster on the Wonder Island",
+        "author": "Jordan Jones",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Happy Knight and the Magic Spell",
+        "author": "Quinn Brown",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Happy Dragon and the Magic Spell",
+        "author": "Alex Jones",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Lucky Monster and the Magic Spell",
+        "author": "Jordan Brown",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Magic Wizard and the Tower of Dreams",
+        "author": "Morgan Jones",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Happy Knight and the Magic Spell",
+        "author": "Casey Jones",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Happy Giant and the Starry Sky",
+        "author": "Quinn Jones",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Magic Princess and the Enchanted Garden",
+        "author": "Riley Wilson",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Happy Pirate and the Tower of Dreams",
+        "author": "Reese Davis",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Happy Monster and the Lost World",
+        "author": "Addison Smith",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Brave Robot and the Starry Sky",
+        "author": "Riley Taylor",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Shiny Pirate and the Secret Forest",
+        "author": "Avery Taylor",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Magic Princess and the Hidden Treasure",
+        "author": "Riley Brown",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Happy Princess and the Moonlight Mystery",
+        "author": "Avery Smith",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Little Pirate and the Tower of Dreams",
+        "author": "Quinn Wilson",
+        "coverPhotoURL": "assets/image7.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Magic Dragon and the Tower of Dreams",
+        "author": "Addison Taylor",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Magic Monster and the Starry Sky",
+        "author": "Riley Williams",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Clever Wizard and the Starry Sky",
+        "author": "Reese Davis",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Brave Wizard and the Starry Sky",
+        "author": "Addison Moore",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Curious Knight on the Wonder Island",
+        "author": "Addison Wilson",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Happy Giant and the Enchanted Garden",
+        "author": "Morgan Johnson",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Happy Princess and the Tower of Dreams",
+        "author": "Quinn Miller",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Happy Wizard and the Enchanted Garden",
+        "author": "Riley Miller",
+        "coverPhotoURL": "assets/image7.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Big Fairy and the Starry Sky",
+        "author": "Casey Taylor",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Magic Wizard and the Great Adventure",
+        "author": "Riley Smith",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Little Fairy and the Enchanted Garden",
+        "author": "Alex Wilson",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Magic Robot and the Hidden Treasure",
+        "author": "Avery Miller",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Big Princess and the Starry Sky",
+        "author": "Casey Johnson",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Shiny Giant and the Secret Forest",
+        "author": "Alex Williams",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Magic Giant and the Hidden Treasure",
+        "author": "Riley Smith",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Adventurous Giant and the Moonlight Mystery",
+        "author": "Jordan Taylor",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Lucky Giant and the Hidden Treasure",
+        "author": "Casey Johnson",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Adventurous Princess and the Enchanted Garden",
+        "author": "Riley Miller",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Happy Fairy and the Lost World",
+        "author": "Morgan Johnson",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Brave Monster and the Secret Forest",
+        "author": "Addison Taylor",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Big Wizard and the Tower of Dreams",
+        "author": "Quinn Johnson",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Lucky Knight and the Lost World",
+        "author": "Jordan Taylor",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Little Monster and the Enchanted Garden",
+        "author": "Quinn Williams",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Happy Wizard and the Moonlight Mystery",
+        "author": "Taylor Jones",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Lucky Robot and the Hidden Treasure",
+        "author": "Avery Smith",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Happy Fairy and the Magic Spell",
+        "author": "Taylor Miller",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Happy Pirate and the Secret Forest",
+        "author": "Casey Williams",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Happy Dragon and the Moonlight Mystery",
+        "author": "Avery Johnson",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Magic Monster and the Moonlight Mystery",
+        "author": "Jordan Jones",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Shiny Monster and the Moonlight Mystery",
+        "author": "Addison Moore",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Brave Fairy and the Hidden Treasure",
+        "author": "Avery Williams",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Brave Giant and the Lost World",
+        "author": "Taylor Smith",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Lucky Monster and the Lost World",
+        "author": "Riley Williams",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Adventurous Pirate and the Enchanted Garden",
+        "author": "Avery Miller",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Happy Princess and the Starry Sky",
+        "author": "Casey Smith",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Lucky Pirate and the Enchanted Garden",
+        "author": "Reese Williams",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Adventurous Pirate and the Hidden Treasure",
+        "author": "Taylor Miller",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Happy Knight and the Lost World",
+        "author": "Jordan Jones",
+        "coverPhotoURL": "assets/image7.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Big Robot and the Secret Forest",
+        "author": "Casey Johnson",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Little Dragon and the Moonlight Mystery",
+        "author": "Casey Jones",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Brave Robot and the Secret Forest",
+        "author": "Taylor Taylor",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Magic Dragon and the Secret Forest",
+        "author": "Riley Williams",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Clever Knight and the Secret Forest",
+        "author": "Avery Williams",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Lucky Knight and the Tower of Dreams",
+        "author": "Morgan Miller",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Adventurous Monster and the Moonlight Mystery",
+        "author": "Reese Brown",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Big Wizard and the Hidden Treasure",
+        "author": "Casey Miller",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Curious Wizard and the Moonlight Mystery",
+        "author": "Alex Smith",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Magic Knight and the Hidden Treasure",
+        "author": "Jordan Williams",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Curious Wizard on the Wonder Island",
+        "author": "Jordan Williams",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Happy Dragon and the Lost World",
+        "author": "Morgan Moore",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Little Princess and the Enchanted Garden",
+        "author": "Reese Williams",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Curious Monster and the Lost World",
+        "author": "Avery Miller",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Brave Fairy and the Magic Spell",
+        "author": "Alex Johnson",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Lucky Unicorn and the Enchanted Garden",
+        "author": "Addison Wilson",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Shiny Dragon and the Lost World",
+        "author": "Taylor Smith",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Adventurous Monster and the Starry Sky",
+        "author": "Casey Taylor",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Big Robot and the Moonlight Mystery",
+        "author": "Morgan Miller",
+        "coverPhotoURL": "assets/image7.webp",
+        "readingLevel": "J"
+    },
+    {
+        "title": "Shiny Knight on the Wonder Island",
+        "author": "Casey Davis",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Brave Fairy and the Hidden Treasure",
+        "author": "Morgan Taylor",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Curious Fairy and the Enchanted Garden",
+        "author": "Riley Miller",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Magic Wizard and the Lost World",
+        "author": "Addison Taylor",
+        "coverPhotoURL": "assets/image9.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Curious Monster and the Great Adventure",
+        "author": "Avery Jones",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Lucky Robot and the Enchanted Garden",
+        "author": "Quinn Johnson",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Lucky Fairy and the Starry Sky",
+        "author": "Quinn Taylor",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Shiny Knight and the Great Adventure",
+        "author": "Alex Smith",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Brave Fairy and the Enchanted Garden",
+        "author": "Avery Wilson",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Adventurous Robot and the Moonlight Mystery",
+        "author": "Reese Wilson",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Clever Princess on the Wonder Island",
+        "author": "Reese Brown",
+        "coverPhotoURL": "assets/image4.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Adventurous Robot and the Magic Spell",
+        "author": "Quinn Jones",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Clever Robot and the Lost World",
+        "author": "Taylor Miller",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "H"
+    },
+    {
+        "title": "Lucky Giant and the Moonlight Mystery",
+        "author": "Quinn Brown",
+        "coverPhotoURL": "assets/image8.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Shiny Robot and the Hidden Treasure",
+        "author": "Alex Smith",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Magic Dragon and the Enchanted Garden",
+        "author": "Avery Brown",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Curious Knight and the Tower of Dreams",
+        "author": "Alex Wilson",
+        "coverPhotoURL": "assets/image7.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Shiny Giant and the Moonlight Mystery",
+        "author": "Alex Wilson",
+        "coverPhotoURL": "assets/image10.webp",
+        "readingLevel": "D"
+    },
+    {
+        "title": "Curious Unicorn and the Lost World",
+        "author": "Quinn Williams",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Happy Unicorn and the Great Adventure",
+        "author": "Casey Brown",
+        "coverPhotoURL": "assets/image2.webp",
+        "readingLevel": "C"
+    },
+    {
+        "title": "Lucky Dragon on the Wonder Island",
+        "author": "Taylor Taylor",
+        "coverPhotoURL": "assets/image3.webp",
+        "readingLevel": "I"
+    },
+    {
+        "title": "Magic Dragon on the Wonder Island",
+        "author": "Jordan Miller",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "F"
+    },
+    {
+        "title": "Happy Giant and the Magic Spell",
+        "author": "Morgan Davis",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "A"
+    },
+    {
+        "title": "Shiny Monster and the Tower of Dreams",
+        "author": "Addison Davis",
+        "coverPhotoURL": "assets/image5.webp",
+        "readingLevel": "G"
+    },
+    {
+        "title": "Brave Pirate and the Enchanted Garden",
+        "author": "Addison Miller",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "B"
+    },
+    {
+        "title": "Curious Monster and the Starry Sky",
+        "author": "Riley Wilson",
+        "coverPhotoURL": "assets/image1.webp",
+        "readingLevel": "E"
+    },
+    {
+        "title": "Lucky Fairy and the Tower of Dreams",
+        "author": "Taylor Brown",
+        "coverPhotoURL": "assets/image6.webp",
+        "readingLevel": "J"
+    }
+]
