3c61d320a5528dc6f8afbc6b77252e30d09ffeb2
jamesmwangi55 <jamesmwangi55@gmail.com>
2024-05-10 10:57:16 +0300

    Implement server and launch

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+    });
+    console.log(`🚀  Server ready at: ${url}`);
+})();
